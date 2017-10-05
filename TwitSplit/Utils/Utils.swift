e746131db0b8bab5d9864c877983e431e9db1ba5
Tri Ngo <ngominhtrint@gmail.com>
2017-10-05 19:37:33 +0700

    Removed regexes approach. Applied recursive to re-calculate total partials

--- TwitSplit/Utils/Utils.swift
+++ TwitSplit/Utils/Utils.swift
@@ -12,71 +12,35 @@ class Utils {
     
     typealias ResultsAndBreakIndex = (results: [String], breakIndex: Int)
     
-    // Using regex but wrong approach
-    static func split(message: String) -> [String] {
-        
-        let error = ["The message contains a span of non-whitespace characters longer than 50 characters"]
-        var hasError = false
-        
-        if message.characters.count < 50 {
-            return [message]
-        }
-        var messageArray = [String]()
-        let messageRegexes = message.match(for: "(\\b.{1,45}(\\s|$))\\s*")
-        if messageRegexes.count > 1 {
-            for (index, string) in messageRegexes.enumerated() {
-                if string.characters.count > 50 {
-                    hasError = true
-                }
-
-                if hasError {
-                    return error
-                } else {
-                    messageArray.append("\(index + 1)/\(messageRegexes.count) \(string.trimmingCharacters(in: .whitespacesAndNewlines))")
-                }
-            }
-        } else {
-            return error
-        }
-        
-        return messageArray
-    }
-    
     static func split(_ message: String, limitCharacters: Int) -> [String] {
         let error = "The message contains a span of non-whitespace characters longer than 50 characters"
         
-        //Return message if the length is less than limit
-        if message.characters.count < limitCharacters {
-            return [message]
-        }
-        
-        //Return error if the length is great than limit and contains non-whitespace
-        if message.characters.count > limitCharacters && !message.contains(" ") {
-            return [error]
-        }
-        
         //Try to estimate total partial before separate
-        var estTotal: Int = (message.characters.count / limitCharacters) + (message.characters.count % limitCharacters > 0 ? 1 : 0)
+        let estTotal: Int = (message.characters.count / limitCharacters) + (message.characters.count % limitCharacters > 0 ? 1 : 0)
         
         //Separate sentences into words by remove whitespaces and new lines
         let words = message.components(separatedBy: .whitespacesAndNewlines)
         
+        let errorWords = words.filter { return $0.characters.count > limitCharacters }
+        
+        //Return message if the length is less than limit
+        guard message.characters.count > limitCharacters else { return [message] }
+        
+        //Return error if the length is great than limit and contains non-whitespace
+        if errorWords.count > 0 { return [error] }
+        
         let resultsAndBreakIndex = joinedPartial(words, estTotal: estTotal, limitCharacters: limitCharacters)
-        var results = resultsAndBreakIndex.results
-    
-        if resultsAndBreakIndex.breakIndex < words.count - 1 {
-            estTotal = estTotal + 1
-            results = joinedPartial(words, estTotal: estTotal, limitCharacters: limitCharacters).results
-        }
-
+        let results = resultsAndBreakIndex.results
+        
         return results
     }
     
     static func joinedPartial(_ words: [String], estTotal: Int, limitCharacters: Int) -> ResultsAndBreakIndex {
         //Initial empty string array to store all partials as output
         var results = [String]()
+        var breakAtIndex = -1
         
-        var breakAtIndex = 0
+        var resultsAndBreakIndex = (results, breakAtIndex)
         
         for i in 0..<estTotal {
             //Add indicator as suffix
@@ -84,41 +48,34 @@ class Utils {
             var length = partial.characters.count
             
             for (index, item) in words.enumerated() {
-                if index < breakAtIndex {
+                if index <= breakAtIndex {
                     continue
                 }
                 
                 // Caculate string length (+ 1 because of whitespace) before joined into partial
                 length += item.characters.count + 1
                 
-                // Break loop if length is over than limit
-                if length > limitCharacters {
-                    breakAtIndex = index
+                // Break loop if length is over than limit, (+ 1 because the last whitespace)
+                if length > limitCharacters + 1 {
                     break
                 } else {
                     partial += item + " "
                 }
+                
+                breakAtIndex = index
             }
             
             //Append partial
             results.append(partial.trimmingCharacters(in: .whitespaces))
         }
         
-        return (results, breakAtIndex)
-    }
-}
-
-extension String {
-    
-    func match(for regex: String) -> [String] {
-        do {
-            let regex = try NSRegularExpression(pattern: regex, options: .caseInsensitive)
-            let nsString = self as NSString
-            let results = regex.matches(in: self, options: .anchored, range: NSRange(location: 0, length: nsString.length))
-            return results.map { nsString.substring(with: $0.range) }
-        } catch let error {
-            print("invalid regex: \(error.localizedDescription)")
-            return []
+        resultsAndBreakIndex = (results, breakAtIndex)
+        
+        if breakAtIndex < words.count - 1 {
+            let total = estTotal + 1
+            resultsAndBreakIndex = joinedPartial(words, estTotal: total, limitCharacters: limitCharacters)
         }
+        
+        return resultsAndBreakIndex
     }
 }
--- TwitSplitTests/MessageSplitTests.swift
+++ TwitSplitTests/MessageSplitTests.swift
@@ -11,6 +11,9 @@ import XCTest
 
 class MessageSplitTests: XCTestCase {
     
+    let limit = 50
+    let error = ["The message contains a span of non-whitespace characters longer than 50 characters"]
+    
     override func setUp() {
         super.setUp()
     }
@@ -19,41 +22,98 @@ class MessageSplitTests: XCTestCase {
         super.tearDown()
     }
     
-    func testMessageLengthLessThanLimit() {
-        //Give
-        let input = "I can't believe Tweeter"
-        let expected = ["I can't believe Tweeter"]
+    func testTweetMessageLengthLessThanMaximum() {
         
-        //When
-        let output = Utils.split(input, limitCharacters: 50)
+        // Give
+        let input = "Hello, Swift"
+        let expected = ["Hello, Swift"]
         
-        //Then
-        XCTAssertEqual(output, expected)
+        // When
+        let output = Utils.split(input, limitCharacters: limit)
+        
+        // Then
+        XCTAssertEqual(output.count, 1, "Should only 1 components")
+        XCTAssertEqual(output, expected, "Short Message should be same the original")
     }
     
-    func testMessageNonWhitespaceAndGreaterThanLimit() {
-        //Give
-        let input = "Ican'tbelieveTweeternowsupportschunkingmymessages,so"
-        let expected = ["The message contains a span of non-whitespace characters longer than 50 characters"]
+    func testTheExampleInAssigment() {
         
-        //When
-        let output = Utils.split(input, limitCharacters: 50)
-        
-        //Then
-        XCTAssertEqual(output, expected)
-    }
-    
-    func testSplitAsPartialWithIndicator() {
-        
-        //Give
+        // Give
         let input = "I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself."
         let expected = ["1/2 I can't believe Tweeter now supports chunking",
                         "2/2 my messages, so I don't have to do it myself."]
         
-        //When
-        let output = Utils.split(input, limitCharacters: 50)
+        // When
+        let output = Utils.split(input, limitCharacters: limit)
+        
+        // Then
+        XCTAssertEqual(output.count, 2, "Should only 2 components")
+        XCTAssertEqual(output, expected, "Same the expectation from the Assignment")
+    }
+    
+    func testAnyMessageLengthExcessThanMaximum() {
+        
+        // Give
+        let input = "Hello, Swiftabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"
+        let expected = error
+        
+        // When
+        let output = Utils.split(input, limitCharacters: limit)
         
         //Then
+        XCTAssertEqual(output, expected)
+    }
+    
+    func testCreateTweetComponentWithNewOneCase() {
+        
+        // Give
+        let input = "Twitter mostly uses Ruby on Rails for their front-end and primarily Scala and Java for back-end services. They use Apache Thrift (originally developed by Facebook) to communicate between different internal services."
+        let expected = ["1/6 Twitter mostly uses Ruby on Rails for their",
+                        "2/6 front-end and primarily Scala and Java for",
+                        "3/6 back-end services. They use Apache Thrift",
+                        "4/6 (originally developed by Facebook) to",
+                        "5/6 communicate between different internal",
+                        "6/6 services."]
+        
+        // When
+        let output = Utils.split(input, limitCharacters: limit)
+        
+        // Then
+        XCTAssertEqual(output, expected, "The last character (Service) didn't add to new component")
+    }
+    
+    func testSplitTweetToMoreThanOneTweet() {
+        
+        // Give
+        let input = "Facebook just switched several of its open source projects — including React — over to the popular MIT license. Before that, Facebook was using their own custom “BSD+Patents” license. This was similar to the widely-used BSD license, but also included a clause that basically said: “you can’t sue Facebook for infringing on your patents. This license came under fire this summer. Here’s what happened."
+        
+        let expected = ["1/9 Facebook just switched several of its open",
+                        "2/9 source projects — including React — over to",
+                        "3/9 the popular MIT license. Before that, Facebook",
+                        "4/9 was using their own custom “BSD+Patents”",
+                        "5/9 license. This was similar to the widely-used",
+                        "6/9 BSD license, but also included a clause that",
+                        "7/9 basically said: “you can’t sue Facebook for",
+                        "8/9 infringing on your patents. This license came",
+                        "9/9 under fire this summer. Here’s what happened."]
+        
+        // When
+        let output = Utils.split(input, limitCharacters: limit)
+        
+        // Then
+        XCTAssertEqual(output, expected, "Split Tweet into more than one Tweet")
+    }
+    
+    func testOneWordIsExcessMaximumCase() {
+        
+        // Give
+        let input = "Facebook just switched several of its open source projects — including ReactReactReactReactReactReactReactReactReactReactReactReactReactReactReact — over to the popular MIT license. Before that, Facebook was using their own custom “BSD+Patents” license. This was similar to the widely-used BSD license, but also included a clause that basically said: “you can’t sue Facebook for infringing on your patents. This license came under fire this summer. Here’s what happened."
+        let expected = error
+        
+        // When
+        let output = Utils.split(input, limitCharacters: limit)
+        
+        // Then
         XCTAssertEqual(output, expected)
     }
     
@@ -61,11 +121,11 @@ class MessageSplitTests: XCTestCase {
         //Give
         let input = "I can't believe Tweeter now supports SwiftSwiftSwiftSwiftSwiftSwift my messages, so I don't have to do it myself."
         let expected = ["1/3 I can't believe Tweeter now supports",
-                        "2/3 SwiftSwiftSwiftSwiftSwiftSwift my messages,",
-                        "3/3 so I don't have to do it myself."]
+                        "2/3 SwiftSwiftSwiftSwiftSwiftSwift my messages, so",
+                        "3/3 I don't have to do it myself."]
         
         //When
-        let output = Utils.split(input, limitCharacters: 50)
+        let output = Utils.split(input, limitCharacters: limit)
         
         //Then
         XCTAssertEqual(output, expected)
@@ -74,19 +134,20 @@ class MessageSplitTests: XCTestCase {
     func testOverThanTenParts() {
         //Give
         let input = "I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself."
-        let expected = ["1/10 I can't believe Tweeter now supports chunking",
-                        "2/10 my messages, so I don't have to do it myself.",
-                        "3/10 I can't believe Tweeter now supports chunking",
-                        "4/10 my messages, so I don't have to do it myself.",
-                        "5/10 I can't believe Tweeter now supports chunking",
-                        "6/10 my messages, so I don't have to do it myself.",
-                        "7/10 I can't believe Tweeter now supports chunking",
-                        "8/10 my messages, so I don't have to do it myself.",
-                        "9/10 I can't believe Tweeter now supports chunking",
-                        "10/10 my messages, so I don't have to do it myself."]
+        let expected = ["1/11 I can't believe Tweeter now supports chunking",
+                        "2/11 my messages, so I don't have to do it myself.",
+                        "3/11 I can't believe Tweeter now supports chunking",
+                        "4/11 my messages, so I don't have to do it myself.",
+                        "5/11 I can't believe Tweeter now supports chunking",
+                        "6/11 my messages, so I don't have to do it myself.",
+                        "7/11 I can't believe Tweeter now supports chunking",
+                        "8/11 my messages, so I don't have to do it myself.",
+                        "9/11 I can't believe Tweeter now supports chunking",
+                        "10/11 my messages, so I don't have to do it",
+                        "11/11 myself."]
         
         //When
-        let output = Utils.split(input, limitCharacters: 50)
+        let output = Utils.split(input, limitCharacters: limit)
         
         //Then
         XCTAssertEqual(output, expected)
@@ -102,7 +163,7 @@ class MessageSplitTests: XCTestCase {
                         "5/5 don't have to do it myself."]
         
         //When
-        let output = Utils.split(input, limitCharacters: 50)
+        let output = Utils.split(input, limitCharacters: limit)
         
         //Then
         XCTAssertEqual(output, expected)
